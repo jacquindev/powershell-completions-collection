2f742edff97b93621ebd64ffae4a0e99768f1913
Jacquin Moon <jacquindev@outlook.com>
2025-03-21 03:45:40 +0700

    feat: add vfox completion file

---
+++ completions/vfox.ps1
@@ -0,0 +1,7 @@
+﻿Register-ArgumentCompleter -Native -CommandName vfox -ScriptBlock {
+	param($commandName, $wordToComplete, $cursorPosition)
+	$other = "$wordToComplete --generate-bash-completion"
+	Invoke-Expression $other | ForEach-Object {
+		[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+	}
+}
